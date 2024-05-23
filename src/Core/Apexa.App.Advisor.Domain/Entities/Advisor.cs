2f2db17b4b6ca5efb6aa046fba4eb46a38673498
olfat-gh <ghasemolfat.it@gmail.com>
2024-05-23 17:07:10 -0400

    Added domain project & Advisor class

---
+++ src/Core/Apexa.App.Advisor.Domain/Entities/Advisor.cs
@@ -0,0 +1,19 @@
+﻿using Apexa.App.Advisor.Domain.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apexa.App.Advisor.Domain.Entities
+{
+    public class Advisor
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Sin { get; set; }
+        public  string Address { get; set; }
+        public string Phone { get; set; }
+        public HealthStatus Status { get; set; }
+    }
+}
---
+++ src/Core/Apexa.App.Advisor.Domain/Enumerations/HealthStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apexa.App.Advisor.Domain.Enumerations
+{
+    public enum HealthStatus
+    {
+        Green,
+        Yellow,
+        Red
+    }
+}
