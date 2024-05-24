80124d5ef513f1d848a3f88324a1cb5a2630dd11
olfat-gh <ghasemolfat.it@gmail.com>
2024-05-24 12:52:48 -0400

    Added Create endpoint

--- src/Presentation/Apexa.AdvisorApp.WebApi/Controllers/V1/AdvisorController.cs
+++ src/Presentation/Apexa.AdvisorApp.WebApi/Controllers/V1/AdvisorController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Apexa.AdvisorApp.Application.Advisors.Commands;
+using Apexa.AdvisorApp.Contracts.V1.Advisor;
+using Apexa.AdvisorApp.Domain.Entities;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Apexa.AdvisorApp.WebApi.Controllers.V1
 {
@@ -6,13 +11,24 @@ namespace Apexa.AdvisorApp.WebApi.Controllers.V1
     public class AdvisorController : ApexaControllerBase
     {
         private readonly ILogger<AdvisorController> _logger;
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
 
-        public AdvisorController(ILogger<AdvisorController> logger) : base(logger)
+        public AdvisorController(ILogger<AdvisorController> logger, IMediator mediator, IMapper mapper) : base(logger)
         {
             _logger = logger;
+            _mediator = mediator;
+            _mapper = mapper;
         }
 
-       
+
+        [HttpPost(Name = "AddAdvisor")]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateAdvisorApiRequest payload)
+        {
+            var command = _mapper.Map<CreateAdvisorCommand>(payload);
+            var id = await _mediator.Send(command);
+            return Ok(id);
+        }
 
 
 
