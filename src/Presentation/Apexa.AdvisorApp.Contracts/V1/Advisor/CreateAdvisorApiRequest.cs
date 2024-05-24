2b8a7d8ffa0584b8dc72d482d10df499c14cdd0c
olfat-gh <ghasemolfat.it@gmail.com>
2024-05-24 12:13:38 -0400

    Added advisor contract project & CreateAdvisorApiRequest

---
+++ src/Presentation/Apexa.AdvisorApp.Contracts/V1/Advisor/CreateAdvisorApiRequest.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Apexa.AdvisorApp.Contracts.V1.Advisor
+{
+    public class CreateAdvisorApiRequest
+    {
+        public required string Name { get; set; }
+        public required string Sin { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
